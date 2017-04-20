a058d44a96e6bea2d6a608f02787ee79a81b5f81
naker90 <AntonioJesusSG96@gmail.com>
2017-04-20 13:04:42 +0100

    Create simple add courses form

--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -1,10 +1,31 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component{
+    constructor(props, context){
+      super(props, context);
+      this.state = {
+        course: { title: null }
+      };
+    }
+
+    onTitleChange(event){
+      let course = this.state.course;
+      course.title = event.target.value;
+      setState({course: course});
+    }
+
     render(){
       return (
         <div>
           <h1>Cursos</h1>
+          <h2>Crear curso</h2>
+          <input type="text"
+              onChange={this.onTitleChange}
+              value={this.state.course.title}/>
+          <br/>
+          <input type="submit"
+              onClick={this.titleSave}
+              value="Guardar"/>
         </div>
       );
     }
