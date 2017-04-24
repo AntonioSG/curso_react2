a61287d37a822f5e5148247bfd5a25d622d030a5
naker90 <AntonioJesusSG96@gmail.com>
2017-04-24 11:01:12 +0100

    Create manager courses form

---
+++ src/components/course/courseForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
+  return (
+    <form>
+      <h2>Administrar cursos</h2>
+      <TextInput
+        name="title"
+        label="Title"
+        value={course.title}
+        onChange={onChange}
+        error={errors.title}/>
+
+      <SelectInput
+        name="authorId"
+        label="Author"
+        value={course.authorId}
+        options={allAuthors}
+        onChange={onChange}
+        error={errors.authorId}/>
+
+      <TextInput
+        name="category"
+        label="Category"
+        value={course.category}
+        onChange={onChange}
+        error={errors.category}/>
+
+      <TextInpu
+        name="length"
+        label="Legth"
+        value={course.length}
+        onChange={onChange}
+        error={errors.length}/>
+
+      <input
+        type="submit"
+        disabled={loading}
+        value={loading ? 'Guardando...' : "Guardar"}
+        className="btn btn-primary"
+        onClick={onSave}/>
+    </form>
+  );
+};
+
+CourseForm.propTypes = {
+  course: React.PropTypes.object.isRequired,
+  allAuthors: React.PropTypes.array,
+  onSave: React.PropTypes.func.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+  loading: React.PropTypes.bool,
+  errors: React.PropTypes.object
+};
+
+export default CourseForm;
