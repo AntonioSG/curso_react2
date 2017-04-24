d12884440c62c4231bd604f80a92adf2af8bced7
naker90 <AntonioJesusSG96@gmail.com>
2017-04-24 13:50:43 +0100

    handlers creates and updates in reducers

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -6,6 +6,18 @@ export default function courseReducer(state = InitialState.courses, action) {
     case type.LOAD_COURSES_SUCCESS:
       return action.courses;
 
+    case type.CREATE_COURSE_SUCCESS:
+      return [
+        ...state,
+        Object.assign({}. action.course)
+      ];
+
+    case type.UPDATE_COURSE_SUCCESS:
+      return [
+        ...state.filter(course => course.id !== action.course.id),
+        Object.assign({}, action.course)
+      ];
+
     default:
       return state;
   }
