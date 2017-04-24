5b16bae84e5dc57861efc8c4ccaf6b8ba06487b3
naker90 <AntonioJesusSG96@gmail.com>
2017-04-24 13:08:48 +0100

    Map author data for dropdown

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import {loadCourses} from './actions/courseAction';
+import {loadAuthors} from './actions/authorAction';
 import routes from './router';
 import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
   <Provider store={store}>
