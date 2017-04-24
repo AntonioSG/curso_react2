e153f3a409078f4c06639f2e47c1de9445bfa2e6
naker90 <AntonioJesusSG96@gmail.com>
2017-04-24 09:34:26 +0100

    Dispatch action on page load

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { render } from 'react-dom';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import {loadCourses} from './actions/courseAction';
 import routes from './router';
 import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
