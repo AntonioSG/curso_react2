3059534a77cbbf51315656b04605f989b522ce2e
naker90 <AntonioJesusSG96@gmail.com>
2017-04-25 12:58:30 +0100

    Hide preloader based on status

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -2,13 +2,13 @@ import * as type from '../actions/actionsTypes';
 import initialState from './initialState';
 
 function actionTypeEndsInSuccess(type) {
-  return type.substring(type.length - 8) === "_SUCCESS";
+  return type.substring(type.length - 8) === '_SUCCESS';
 }
 
-export default function ajaxStatusReducer(status = initialState.numberAjaxCallInProgress, action) {
+export default function ajaxStatusReducer(state = initialState.numberAjaxCallInProgress, action) {
   if(action.type ===  type.BEGIN_AJAX_CALL){
     return state + 1;
-  }else if(actionTypeEndsInSuccess(action.type())){
+  }else if (actionTypeEndsInSuccess(action.type)) {
     return state - 1;
   }
 
