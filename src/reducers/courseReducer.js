afb762e98f081ba32976da095cada88324290714
naker90 <AntonioJesusSG96@gmail.com>
2017-05-05 13:59:54 +0100

    Setup Webpack

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,6 +2,7 @@ import * as type from '../actions/actionsTypes';
 import InitialState from './initialState';
 
 export default function courseReducer(state = InitialState.courses, action) {
+
   switch (action.type){
     case type.LOAD_COURSES_SUCCESS:
       return action.courses;
